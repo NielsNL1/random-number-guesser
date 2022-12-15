c2e8d5c08ab32b0ca132915571638e9a12b3bbe6
Niels <111445696+NielsNL1@users.noreply.github.com>
2022-12-15 14:37:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import random
 
 # A random number guessing script.
-# Idea is from https://www.upgrad.com/blog/python-projects-ideas-topics-beginners/
 user_input = input('guess the number 1-10 ')
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 random_number = random.choice(number_list)
